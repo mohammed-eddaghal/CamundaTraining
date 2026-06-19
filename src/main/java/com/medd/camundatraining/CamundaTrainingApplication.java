583c162584cebb980d03668b744b609db0b6aa80
mohammed eddaghal <mohammededdaghal@gmail.com>
2026-06-19 13:07:54 +0100

    setup project with camunda dependency and H2 db

---
+++ src/main/java/com/medd/camundatraining/CamundaTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.medd.camundatraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medd/camundatraining/CamundaTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medd.camundatraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
